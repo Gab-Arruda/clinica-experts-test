fbe66df8ce3a603bce38a5b1cb06f9999db45826
gasantos <gasantos@nelogica.com.br>
2023-08-26 20:05:03 -0300

    Adiciona componente de filter e metrics

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,11 +5,13 @@ import axios from 'axios';
 // import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
+import Metrics from './components/Metrics.vue'
+import Filter from './components/Filter.vue'
 
 const links_list = ref([]);
-const app_metrics = ref({});
 const order_type = ref('desc');
 const filter_column = ref('created_at');
+const filter_modal = ref(false);
 
 const removeLink = (link) => {
   // chamar rota para deletar o link
@@ -23,8 +25,15 @@ const editLink = (link) => {
   console.log('editou o link: ' + link.url);
 }
 
+const setFilterModal = () => {
+  // seta alguma ref pra abrir o modal de filter
+  filter_modal.value = !filter_modal.value;
+  console.log('setFilterModal: ' + filter_modal.value);
+}
+
 const setOrderType = () => {
   order_type.value = order_type.value == 'asc' ? 'desc' : 'asc';
+  console.log('setOrderType: ' + order_type.value);
   getLinksList();
 }
 
@@ -40,12 +49,7 @@ onMounted(() => {
   axios.get('http://localhost:8000/api/links/', { params: {filterByColumn : "created_at", filterOrderType: "desc"}})
   .then(response => {
     links_list.value = response.data;
-  });
-
-  // Métricas do app
-  axios.get('http://localhost:8000/api/links/metrics')
-  .then(response => {
-    app_metrics.value = response.data;
+    console.log('app mounted');
   });
 })
 
@@ -54,36 +58,9 @@ onMounted(() => {
 <template>
   <main class="app bg-slate-200 flex flex-col justify-between h-screen">
     <Header></Header>
-    <body class="px-16">        
-      <section class="my-8">
-        <ul class="flex flex-row flex-wrap items-center justify-around">
-          <li class="flex m-4">
-            <img src="./assets/link.png" alt="link-icon" class="w-6 h-6">
-            <div class="flex flex-col max-w-[100px]">
-              <span class="text-center font-bold text-3xl">{{ app_metrics.links_amount }}</span>
-              <span class="text-center font-bold text-slate-500">Links</span>
-            </div>
-          </li>
-          <li class="flex m-4">
-            <img src="./assets/view.png" alt="view-icon" class="w-6 h-6">
-            <div class="flex flex-col max-w-[100px]">
-              <span class="text-center font-bold text-3xl">{{ app_metrics.requisitions_amount }}</span>
-              <span class="text-center font-bold text-slate-500">All time views</span>
-            </div>
-          </li>
-          <li class="flex m-4">
-            <img src="./assets/30-days.png" alt="30-days-icon" class="w-6 h-6">
-            <div class="flex flex-col max-w-[100px]">
-              <span class="text-center font-bold text-3xl">{{ app_metrics.requisitions_this_month }}</span>
-              <span class="text-center font-bold text-slate-500">Views this month</span>
-            </div>
-          </li>
-        </ul>
-      </section>
-      <section class="flex items-center justify-center">
-        <img src="./assets/two-arrow.png" alt="two-arrow-icon" class="w-8 h-8 mx-4 cursor-pointer" @click="setOrderType()">
-        <img src="./assets/filter.png" alt="filter-icon" class="w-8 h-8 mx-4 cursor-pointer" @click="editLink(link)">
-      </section>
+    <body class="px-16 grow">        
+      <Metrics></Metrics>
+      <Filter @setOrderType="setOrderType" @setFilterModal="setFilterModal"></Filter>
       <section class="links-list my-4">
         <ul class="flex-col my-4">
           <li v-for="link in links_list" :key="link.id" class="flex justify-between flex-wrap items-center my-3 bg-white px-8 py-4 rounded-md">
---
+++ front/src/components/Filter.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+defineEmits(['setOrderType','setFilterModal'])
+
+</script>
+
+<template>
+    <section class="flex items-center justify-center">
+        <img src="../assets/two-arrow.png" alt="two-arrow-icon" class="w-8 h-8 mx-4 cursor-pointer" @click="$emit('setOrderType')">
+        <img src="../assets/filter.png" alt="filter-icon" class="w-8 h-8 mx-4 cursor-pointer" @click="$emit('setFilterModal')">
+    </section>
+</template>
+
+<style scoped>
+
+</style>
